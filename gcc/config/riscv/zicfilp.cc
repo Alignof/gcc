93fd905a6a7a187d25622b399322ab18841f64be
Alignof <alignof@outlook.com>
2023-11-11 00:11:15 +0900

    [add] add `make_pass_insert_lpad` to zicfilp.cc

--- gcc/config/riscv/zicfilp.cc
+++ gcc/config/riscv/zicfilp.cc
@@ -16,6 +16,9 @@
 
    You should have received a copy of the GNU General Public License
    along with GCC; see the file COPYING3.  If not see
-   <http://www.gnu.org/licenses/>.  
+   <http://www.gnu.org/licenses/>.
 */
 
+rtl_opt_pass *make_pass_insert_lpad(gcc::context *ctxt) {
+  return new pass_insert_lpad(ctxt);
+}
